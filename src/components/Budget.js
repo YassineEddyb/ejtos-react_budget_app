fb6401387e2d87108990bd8be8d3a6e3c58181ae
yassine ed-dyb <75098144+YassineEddyb@users.noreply.github.com>
2023-06-30 22:34:23 +0000

    updating app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const [cost, setCost] = useState(200);
+
+    alert("you connot reduce the budget lower than the spending");
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                    required='required'
+                    type='number'
+                    id='cost'
+                    value={cost}
+                    style={{ marginLeft: '2rem' , size: 10}}
+                    onChange={(event) => setCost(event.target.value)}
+                />
+            </span>
         </div>
     );
 };
